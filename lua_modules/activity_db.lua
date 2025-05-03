410f6357c46be90da9d36502bd69373b51932d8f
twincannon <twincannon@gmail.com>
2025-05-03 00:46:32 -0400

    Update activity_db generation from latest go script

--- lua_modules/activity_db.lua
+++ lua_modules/activity_db.lua
@@ -3,7 +3,26 @@ local activity_db = {}
 activity_db.Records = {
 	[Zone.freporte] = {
 		{
-			Annoucement = "Would anyone be willing to assist in cutting down [trees]?",
+			ActivityName = "ChopTreesForBarkeep",
+			Announcement = "Would anyone be willing to assist in cutting down [trees]?",
+			Hail = "Hail stranger",
+			Keyword = "trees",
+			Request = "Here is what you need to do",
+			QuestType = 0,
+			QuestItemID = 12345,
+			ObjectiveModelType = 0,
+			ObjectiveModel = "stu",
+			ObjectiveLocations = {
+				{ X = 100, Y = 200, Z = 300, },
+				{ X = 400, Y = 500, Z = 600, },
+			},
+			Result = "Thanks for doing the activity",
+			RewardTier = 1,
+		},
+		{
+			ActivityName = "test2",
+			Announcement = "I need more [cowbell]",
+			Keyword = "cowbell",
 		},
 	},
 
