51e03cd07a41a203795d740d820c664b044f1a90
twincannon <twincannon@gmail.com>
2025-05-11 00:04:08 -0400

    Improve biting trap script by not activating on clients or pets and some other safeguards

--- global/RngSwarmPetBitingTrap.lua
+++ global/RngSwarmPetBitingTrap.lua
@@ -13,12 +13,20 @@ function event_timer(e)
 	if e.timer == trap_timer then
 		local npc_list = e.self:GetCloseMobList(trigger_distance) -- eq.get_entity_list():GetNPCList()
 		local owner = e.self:GetOwner()
-		local best_dist = 999999
+
+		if owner == nil then
+			eq.depop()
+			return
+		end
+
+		local best_dist = trigger_distance + 1
 		local best_npc_id = -1
 		for npc in npc_list.entries do
-			if npc.valid and npc:GetID() ~= e.self:GetID() and npc:GetID() ~= owner:GetID() then
+			-- Don't activate on clients, bots, mercs, or friendly pets (not checking npc:IsPet() because it returns false for swarm pets)
+			local is_client_or_clients_pet = npc:IsOfClientBotMerc() or (npc:GetOwner() and npc:GetOwner():IsOfClientBotMerc())
+			if npc.valid and npc:GetID() ~= e.self:GetID() and npc:GetID() ~= owner:GetID() and is_client_or_clients_pet == false and e.self:CheckLoS(npc) then
 				local dist = e.self:CalculateDistance(npc:GetX(), npc:GetY(), npc:GetZ())
-				if dist < trigger_distance and dist < best_dist then
+				if dist <= trigger_distance and dist < best_dist then
 					best_dist = dist
 					best_npc_id = npc:GetID()
 				end
@@ -26,7 +34,7 @@ function event_timer(e)
 		end
 
 		if best_npc_id ~= -1 and owner.valid then
-			owner:CastSpell(spell_id, best_npc_id)
+			owner:CastSpell(spell_id, best_npc_id) -- having owner cast the spell so the dot works
 			eq.stop_timer(trap_timer)
 			eq.set_timer(depop_timer, 100) -- delay depopping so the spell can be successfully cast
 		end
